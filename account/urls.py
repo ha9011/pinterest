c23a0dca47d696c46b8aede2041fda451518ac8d
하동 <ha90111@naver.com>
2021-04-01 09:31:23 +0900

    add delete accounts

--- account/urls.py
+++ account/urls.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
-from .views import hello_world, AccountCreateView, AccountDetailView,AccountUpdateView
+from .views import hello_world, AccountCreateView, AccountDetailView, AccountUpdateView, AccountDeleteView
 
 app_name = "account"
 
@@ -12,5 +12,6 @@ urlpatterns = [
     path("logout/", LogoutView.as_view(), name="logout" ),
     path("create/", AccountCreateView.as_view(), name="create" ), # 계정 생성
     path("update/<int:pk>", AccountUpdateView.as_view(), name="update" ), # 계정 수정
-    path("detail/<int:pk>", AccountDetailView.as_view(), name="detail")  # 계정 정보
+    path("detail/<int:pk>", AccountDetailView.as_view(), name="detail") , # 계정 정보
+    path("delete/<int:pk>", AccountDeleteView.as_view(), name="delete")  # 계정 삭제
 ]
--- account/views.py
+++ account/views.py
@@ -5,28 +5,28 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.urls import reverse_lazy
-from django.views.generic import CreateView, DetailView, UpdateView
+from django.views.generic import CreateView, DetailView, UpdateView, DeleteView
 
 from account.forms import AccountUpdateForm
 
 
 def hello_world(request):
     qwe = "q"
-    #return HttpResponse("hello world!")
+    # return HttpResponse("hello world!")
     return render(request, "account/hello_world.html")  # templates in setting 설정
 
 
 class AccountCreateView(CreateView):
-    model = User   # 모델 기본제공
-    form_class = UserCreationForm # 기본제공
+    model = User  # 모델 기본제공
+    form_class = UserCreationForm  # 기본제공
     success_url = reverse_lazy("account:hello_world")  # 성공시 리다리엑트
     # reverse Vs reverse_lazy 차이는 함수 // 클레스 차이
     template_name = "account/create.html"  # 현재 템플릿(아이디 생성할..)
 
 
 class AccountUpdateView(UpdateView):
-    model = User   # 모델 기본제공
-    form_class = AccountUpdateForm # 커스텀
+    model = User  # 모델 기본제공
+    form_class = AccountUpdateForm  # 커스텀
     success_url = reverse_lazy("account:hello_world")  # 성공시 리다리엑트
     # reverse Vs reverse_lazy 차이는 함수 // 클레스 차이
     template_name = "account/update.html"  # 현재 템플릿(아이디 생성할..)
@@ -34,5 +34,12 @@ class AccountUpdateView(UpdateView):
 
 class AccountDetailView(DetailView):
     model = User
-    context_object_name = 'target_user' # model을 다르게 해야한다, 똑같이 user로 할 경우 로그인 세션의 user로 인식됨
+    context_object_name = 'target_user'  # model을 다르게 해야한다, 똑같이 user로 할 경우 로그인 세션의 user로 인식됨
     template_name = "account/detail.html"
+
+
+class AccountDeleteView(DeleteView):
+    model = User
+
+    success_url = reverse_lazy("account:login")  # 성공시 리다리엑트
+    template_name = "account/delete.html"
