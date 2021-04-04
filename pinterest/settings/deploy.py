dbfd27f9f83fec9a251e4ac21e2acf5067982c4c
하동 <ha90111@naver.com>
2021-04-04 17:57:21 +0900

    docker secret

--- pinterest/settings/deploy.py
+++ pinterest/settings/deploy.py
@@ -1,6 +1,13 @@
 
 from .base import *
 
+def read_secret(secret_name):
+    file = open("/run/secrets/"+secret_name)
+    secret = file.read()
+    secret = secret.rstrip().lstrip()
+    file.close()
+    return secret
+
 # reading .env file
 environ.Env.read_env(
     env_file=os.path.join(BASE_DIR, '.env')
@@ -11,7 +18,7 @@ environ.Env.read_env(
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env('SECRET_KEY')
+SECRET_KEY = read_secret('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # 배포에서는 False로
@@ -28,7 +35,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'django',  # db의 이름
         'USER': 'django', #
-        'PASSWORD': 'password1234',
+        'PASSWORD': read_secret("MYSQL_PASSWORD"),
         'HOST': 'mariadb',   # 컨테이너 이름
         'PORT': '3306',
     }
